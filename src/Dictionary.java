8fb3c7bc7fa8366996efeef8a920bf99ab70d93e
Dan <stickbugdanny@gmail.com>
2024-03-07 00:12:19 +0200

    Update Dictionary.java

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,7 +1,5 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.awt.*;
+import java.io.*;
 import java.math.BigDecimal;
 import java.util.*;
 
@@ -14,25 +12,32 @@ public class Dictionary {
     TreeMap<String, TreeMap<String, TreeSet<Integer>>> dictionary;
 
     // Variables to store statistics about the collection and dictionary
-    private int wordsInCollection;
-    private int wordsInDictionary;
-    private int sizeCollection;
-    private long sizeDictionary;
+    private int collectionWordCount;
+    private int dictionaryWordCount;
+    private int collectionSize;
+    private double dictionarySize;
+
+    private String collectionLocation;
+
+    private String dictionaryLocation;
+
 
     /**File references array*/
     File[] textFiles;
 
-    Dictionary() throws Exception {
+    Dictionary(String collPath, String dictPath) throws Exception {
 
         // Initialize data structures
         dictionary = new TreeMap<>();
-        wordsInCollection = 0;
-        wordsInDictionary = 0;
-        sizeCollection = 0;
-        sizeDictionary = 0;
+        collectionWordCount = 0;
+        dictionaryWordCount = 0;
+        collectionSize = 0;
+        dictionarySize = 0;
+        collectionLocation = collPath;
+        dictionaryLocation = dictPath;
 
         //files folder path
-        File folder = new File("src/res");
+        File folder = new File(collectionLocation);
 
         //Get files from folder
         File[] allFiles = folder.listFiles();
@@ -55,7 +60,7 @@ public class Dictionary {
     /**Build the inverted index dictionary*/
     public void buildDictionary() throws Exception {
         for (int i = 0; i < textFiles.length; i++) {
-            sizeCollection += textFiles[i].length();
+            collectionSize += textFiles[i].length();
             FictionBook fictionBook = new FictionBook(textFiles[i]);
 
             // Process author names
@@ -63,7 +68,7 @@ public class Dictionary {
                 String author = person.getFullName();
                 StringTokenizer stringTokenizer = new StringTokenizer(author);
                 while (stringTokenizer.hasMoreTokens()) {
-                    wordsInCollection++;
+                    collectionWordCount++;
                     String s = stringTokenizer.nextToken().toLowerCase();
                     TreeMap<String, TreeSet<Integer>> wordMap = dictionary.get(s);
                     if (wordMap == null) {
@@ -79,11 +84,11 @@ public class Dictionary {
                 }
             }
 
-            // Process title
+            //Process title
             String title = fictionBook.getTitle();
             StringTokenizer stringTokenizer = new StringTokenizer(title);
             while (stringTokenizer.hasMoreTokens()) {
-                wordsInCollection++;
+                collectionWordCount++;
                 String s = stringTokenizer.nextToken().toLowerCase();
                 TreeMap<String, TreeSet<Integer>> wordMap = dictionary.get(s);
                 if (wordMap == null) {
@@ -104,7 +109,7 @@ public class Dictionary {
                     String text = element.getText();
                     stringTokenizer = new StringTokenizer(text, ".,:;()[]{}<>_- —=+“”'`\"/|!?$^&*@#%0123456789");
                     while (stringTokenizer.hasMoreTokens()) {
-                        wordsInCollection++;
+                        collectionWordCount++;
                         String s = stringTokenizer.nextToken().toLowerCase();
                         TreeMap<String, TreeSet<Integer>> wordMap = dictionary.get(s);
                         if (wordMap == null) {
@@ -122,14 +127,11 @@ public class Dictionary {
             }
 
         }
-        wordsInDictionary = dictionary.size();
+        dictionaryWordCount = dictionary.size();
 
         // Write dictionary information to a file
-        PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("src/results/dictionary.txt")));
-        printWriter.write("Collection size: " + sizeCollection + " bytes;\n");
-        printWriter.write("Words in collection: " + wordsInCollection + ";\n");
-        printWriter.write("Words in dictionary: " + wordsInDictionary + ";\n");
-        printWriter.write("\n \n");
+        PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(dictionaryLocation)));
+        printWriter.write("|||||| Zoned Dictionary ||||||\n");
         for (String word : dictionary.keySet()) {
             printWriter.println(word);
             for (String group : dictionary.get(word).keySet()) {
@@ -138,55 +140,60 @@ public class Dictionary {
         }
         printWriter.close();
 
-        // Update size of the dictionary file
-        File output = new File("dictionary.txt");
-        sizeDictionary = output.length();
+        //Update size of the dictionary file
+        File output = new File(dictionaryLocation);
+        long fileSizeBytes = output.length(); // File size in bytes
 
-        // Append dictionary size information to the same file
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("src/results/dictionary.txt", true)));
-        out.println("\nDICTIONARY SIZE: " + sizeDictionary + " bytes \n");
-        out.flush();
-        out.close();
+        //Convert bytes to kilobytes
+        dictionarySize = (double) fileSizeBytes / 1024;
     }
 
     // Method to perform a query-based search using zone ranking
     public void search(String title, String author, String body) {
+        String searchTitle = title.toLowerCase();
+        String searchAuthor = author.toLowerCase();
+        String searchBody = body.toLowerCase();
+
         ArrayList<Integer> authorDocs = new ArrayList<>();
         ArrayList<Integer> titleDocs = new ArrayList<>();
         ArrayList<Integer> bodyDocs = new ArrayList<>();
 
         try {
-
-            // Process title query
-            if (title.equals("")) {
+            if (searchTitle.equals("")) {
                 for (int i = 0; i < textFiles.length; i++) {
                     titleDocs.add(i);
                 }
-            } else {
-                titleDocs.addAll(dictionary.get(title).get("title"));
+            }
+            else {
+                for (Integer docID : dictionary.get(searchTitle).get("title")) {
+                    titleDocs.add(docID);
+                }
             }
 
-            // Process author query
-            if (author.equals("")) {
+            if (searchAuthor.equals("")) {
                 for (int i = 0; i < textFiles.length; i++) {
                     authorDocs.add(i);
                 }
-            } else {
-                authorDocs.addAll(dictionary.get(author).get("author"));
             }
-
-            // Process body query
-            if (body.equals("")) {
+            else {
+                for (Integer docID : dictionary.get(searchAuthor).get("author")) {
+                    authorDocs.add(docID);
+                }
+            }
+            if (searchBody.equals("")) {
                 for (int i = 0; i < textFiles.length; i++) {
                     bodyDocs.add(i);
                 }
-            } else {
-                bodyDocs.addAll(dictionary.get(body).get("body"));
             }
-
-        } catch (NullPointerException e) {
-            System.out.println("No documents for the specified search have been found.");
+            else {
+                for (Integer docID : dictionary.get(searchBody).get("body")) {
+                    bodyDocs.add(docID);
+                }
+            }
+        } catch(NullPointerException e){
+            System.out.println("No files found in the specified folder.");
         }
+
 
         // TreeMap to store the score for each document
         TreeMap<Integer, BigDecimal> map = new TreeMap<>();
@@ -207,16 +214,63 @@ public class Dictionary {
             map.put(docID, map.get(docID).add(BigDecimal.valueOf(0.1)));
         }
 
-        // Array of predefined values to determine the relevance order
+        //Array of predefined values to determine the relevance order
         BigDecimal[] values = {BigDecimal.valueOf(1.0), BigDecimal.valueOf(0.9), BigDecimal.valueOf(0.7),
                 BigDecimal.valueOf(0.6), BigDecimal.valueOf(0.4), BigDecimal.valueOf(0.3), BigDecimal.valueOf(0.1)};
 
-        // Print the documents in order of relevance for each value
+        //Print the documents in order of relevance for each value
         for (BigDecimal value : values)
             for (Integer i : map.keySet())
                 if (map.get(i).equals(value))
                     System.out.println(i + " " + map.get(i));
 
         System.out.println();
+    }
+
+    public String getDictPath(){
+        return dictionaryLocation;
+    }
+    public String getCollPath(){
+        return collectionLocation;
+    }
+    public int getCollectionWordCount(){
+        return collectionWordCount;
+    }
+    public int getDictionaryWordCount(){
+        return dictionaryWordCount;
+    }
+    public int getCollectionSize(){
+        return collectionSize;
+    }
+    public double getDictionarySize() {
+        return dictionarySize;
+    }
+
+    public void printStats() throws IOException {
+        PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("src/results/stats.txt")));
+        printWriter.write("Collection size: " + collectionSize/1024 + " kb;\n");
+        printWriter.write("Dictionary size: " + dictionarySize + " kb;\n");
+        printWriter.write("Words in collection: " + collectionWordCount + ";\n");
+        printWriter.write("Words in dictionary: " + dictionaryWordCount + ";\n");
+
+        printWriter.close();
+    }
+
+    /**Open .txt files*/
+    public void openTXT(String filePath) throws IOException {
+        File file = new File(filePath);
+
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            System.out.println("\nPulling up the file...");
+
+            if (file.exists()) {
+                desktop.open(file);
+            } else {
+                System.out.println("File not found: " + filePath + "; Please restart the program.");
+            }
+        } else {
+            System.out.println("Desktop is not supported.");
+        }
     }
 }
